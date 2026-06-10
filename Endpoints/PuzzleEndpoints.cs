803143ecb6f8819d3086d55cc447c03a791993af
Xenon <sina274621@gmail.com>
2026-06-10 22:50:37 +0330

    Change the stat endpoint to /stats

--- Endpoints/PuzzleEndpoints.cs
+++ Endpoints/PuzzleEndpoints.cs
@@ -15,7 +15,7 @@ public static class PuzzleEndpoints
         // ------------Player----------------------------------------------------------
         group.MapGet("/daily", GetDailyPuzzle.Handler);
         group.MapPost("/{id}/guess", Guess.Handler);
-        group.MapGet("/{id}/stats", GetStatsHandler.Handler);
+        group.MapGet("/stats", GetStatsHandler.Handler);
 
         // ------------Admin-----------------------------------------------------------
         group.MapGet("/", GetPuzzles.Handler)
